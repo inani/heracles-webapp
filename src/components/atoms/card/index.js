74e5c9eb7590ef0716925e2b853e08e1f53a502f
aichnerc <christian.aichner@edu.htl-villach.at>
2019-03-01 16:27:19 +0100

    Introduced Hero, Nav and Intro molecules and improved button atom

---
+++ src/components/atoms/card/index.js
@@ -0,0 +1,3 @@
+import * as React from 'react'
+import classnames from 'classnames/bind'
+
--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -9,15 +9,17 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 
 // organisms
-import TestDaten from "../organisms/TestDaten";
+import Btn from "../atoms/button";
+import Intro from "../organisms/intro";
 
-import Btn from "../atoms/button/index.js";
+let navitems = [{href:"/testpage", text:"Test", active:true},{href:"/furtherpage", text:"Next One", active:false}];
 
 // Rendering of all active organisms
 class Homepage extends Component {
   render() {
     return (
       <main className="Homepage">
+      <Intro items={navitems}/>
         <header className="Homepage-header">
           <img src={logo} className="Homepage-logo" alt="logo" />
           <p>

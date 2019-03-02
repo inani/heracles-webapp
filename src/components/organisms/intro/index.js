883e4a19524778b303d2d5948970499787f54df8
christianaichner <contact@aichner-christian.com>
2019-03-02 22:10:23 +0100

    Create new carousel hero + removed old hero + cleaned up molecules

--- src/components/organisms/intro/index.js
+++ src/components/organisms/intro/index.js
@@ -4,13 +4,13 @@ import * as React from 'react'
 import Btn from "../../atoms/button"
 
 import Nav from "../../molecules/nav";
-import Hero from "../../molecules/hero";
+import Hero from "../../molecules/hero-carousel";
 import HeroContent from "../../molecules/hero_content";
 
 
 
 const Intro = (props: Props): React.Element<*> => {
-    const { items } = props;
+    const { navitems, heroitems } = props;
 
     function GetState(active) {
         if (active) {
@@ -31,14 +31,12 @@ const Intro = (props: Props): React.Element<*> => {
     const App = () => [
         <Nav theme="D">
         <ul class="navbar-nav ml-auto d-flex justify-content-center align-items-center">
-            {items.map((item, i) => {
+            {navitems.map((item, i) => {
             return (<li key={i} className={GetState(item.active)} data-id={i}><a className={GetType(item.type)} href={item.href}>{item.text}</a></li>) 
             })}
         </ul>
        </Nav>,
-       <Hero>
-            <HeroContent/>
-       </Hero>
+       <Hero heroitems={heroitems}/>
     ];
 
     return (

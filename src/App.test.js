d720ddd6be692ac9bb6647e0c3584b289b0dde54
Florian Kleber <kleberbaum@erebos.xyz>
2019-03-03 15:47:18 +0100

    Revert "create atomic design structure and test apollo client"

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
-// standard libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// other libs
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-// standard components
-import App from "./App";
-
-// Test if rendering properly
-it("renders without crashing", () => {
-  const div = document.createElement("div");
+it('renders without crashing', () => {
+  const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
